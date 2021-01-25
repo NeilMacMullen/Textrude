c840ee5bae9fbbafc4574728c0ee47b6b1623507
NeilMacMullen <neil.macmullen@outlook.com>
2021-01-25 13:24:14 +0000

    Moves the scriptlibrary to a shared project so it can be more easily copied to output folder when under debug

---
+++ ScriptLibrary/Dummy.cs
@@ -0,0 +1,13 @@
+﻿namespace ScriptLibrary
+{
+    /// <summary>
+    ///     Dummy class - the purpose of this project is simply to provide a way to pull the standard scripts into
+    ///     the output folder of the application projects
+    ///     TODO - we probably don't even need to build any code - see
+    ///     https://weblogs.asp.net/leftslipper/creating-visual-studio-projects-that-only-contain-static-files
+    ///     which suggest that by tweaking the project file we can avoid this bit
+    /// </summary>
+    public class Dummy
+    {
+    }
+}
