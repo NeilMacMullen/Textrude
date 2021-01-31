895a801043782c1c19fc40f5f9b3f33fc13cfdbc
NeilMacMullen <neil.macmullen@outlook.com>
2021-01-31 14:29:15 +0000

    Introduce settings mechanism

--- TextrudeInteractive/MainWindow.xaml.cs
+++ TextrudeInteractive/MainWindow.xaml.cs
@@ -43,11 +43,14 @@ namespace TextrudeInteractive
 
         private bool _wordWrapOn;
 
+
         public MainWindow()
         {
             InitializeComponent();
+
             templateFileBar.OnSave = () => TemplateTextBox.Text;
-            templateFileBar.OnLoad = OnTemplatedLoadedFromFile;
+            templateFileBar.OnLoad = (text, _) => TemplateTextBox.Text = text;
+            ;
             SetTitle(string.Empty);
             _modelManager = new("model", InputModels, p => p.OnUserInput = OnModelChanged);
             _outputManager = new("output", OutputTab, _ => { });
@@ -67,9 +70,78 @@ namespace TextrudeInteractive
                 .ObserveOn(SynchronizationContext.Current)
                 .Subscribe(HandleRenderResults);
             _uiIsReady = true;
+
+            LoadSettings();
             RunBackgroundUpgradeCheck();
+
+
             DataContext = this;
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RunBackgroundUpgradeCheck()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    _latestVersion = await UpgradeManager.GetLatestVersion();
+                    await Task.Delay(TimeSpan.FromHours(24));
+                }
+            });
+        }
+
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+        {
+            _inputStream.OnCompleted();
+            PersistSettings();
+        }
+
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e) => _mainEditWindow.Register();
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        #region outputs menu
+
+        private void AddOutput(object sender, RoutedEventArgs e) => _outputManager.AddPane();
+
+        private void RemoveOutput(object sender, RoutedEventArgs e) => _outputManager.RemoveLast();
+
+
+        private void SaveAllOutputs(object sender, RoutedEventArgs e)
+        {
+            _outputManager.ForAll(p => p.SaveIfLinked());
+        }
+
+        #endregion
+
+        #region inputs menu
+
+        private void AddModel(object sender, RoutedEventArgs e) =>
+            _modelManager.AddPane();
+
+        private void RemoveModel(object sender, RoutedEventArgs e) => _modelManager.RemoveLast();
+
+        private void ReloadAllInputs(object sender, RoutedEventArgs e)
+        {
+            _modelManager.ForAll(p => p.LoadIfLinked());
+            templateFileBar.LoadIfLinked();
+        }
+
+        private void SaveAllInputs(object sender, RoutedEventArgs e)
+        {
+            _modelManager.ForAll(p => p.SaveIfLinked());
+            templateFileBar.SaveIfLinked();
+        }
+
+        #endregion
+
+        #region view menu
 
         public bool LineNumbersOn
         {
@@ -101,24 +173,32 @@ namespace TextrudeInteractive
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private void SmallerFont(object sender, RoutedEventArgs e) => TextSize = Math.Max(TextSize - 2, 10);
 
-        private void OnTemplatedLoadedFromFile(string text, bool isNewFile)
+        private void LargerFont(object sender, RoutedEventArgs e) => TextSize = Math.Min(TextSize + 2, 36);
+
+        private void ToggleLineNumbers(object sender, RoutedEventArgs e) => LineNumbersOn = !LineNumbersOn;
+
+        private void ToggleWordWrap(object sender, RoutedEventArgs e) => WordWrapOn = !WordWrapOn;
+
+        #endregion
+
+        #region project menu
+
+        public void SetOutputPanes(EngineOutputSet outputControl)
         {
-            TemplateTextBox.Text = text;
-        }
-
-
-        private void RunBackgroundUpgradeCheck()
-        {
-            Task.Run(async () =>
+            _outputManager.Clear();
+            foreach (var f in outputControl.Outputs)
             {
-                while (true)
-                {
-                    _latestVersion = await UpgradeManager.GetLatestVersion();
-                    await Task.Delay(TimeSpan.FromHours(24));
-                }
-            });
+                var pane = _outputManager.AddPane();
+                pane.Format = f.Format;
+                pane.OutputPath = f.Path;
+                pane.OutputName = f.Name;
+            }
+
+            //ensure there is always at least one output - otherwise things can get confusing for the user
+            if (!_outputManager.Panes.Any())
+                _outputManager.AddPane();
         }
 
         public void SetTitle(string path)
@@ -130,6 +210,66 @@ namespace TextrudeInteractive
                 $"Textrude Interactive {GitVersionInformation.SemVer} : {file}";
             Title = title;
 #endif
+        }
+
+        public EngineOutputSet CollectOutput()
+        {
+            return new(
+                _outputManager.Panes.Select(b => new OutputPaneModel(b.Format, b.Name, b.OutputPath))
+            );
+        }
+
+        private void LoadProject(object sender, RoutedEventArgs e) => _projectManager.LoadProject();
+
+
+        private void SaveProject(object sender, RoutedEventArgs e) => _projectManager.SaveProject();
+
+
+        private void SaveProjectAs(object sender, RoutedEventArgs e) => _projectManager.SaveProjectAs();
+
+        public void SetUi(EngineInputSet gi)
+        {
+            DefinitionsTextBox.Text = string.Join(Environment.NewLine, gi.Definitions);
+            _modelManager.Clear();
+            foreach (var giModel in gi.Models)
+            {
+                var pane = _modelManager.AddPane();
+                pane.Format = giModel.Format;
+                pane.Text = giModel.Text;
+                pane.ModelName = giModel.Name;
+                pane.ModelPath = giModel.Path;
+            }
+
+            //ensure we start with at least one model to avoid confusing the user
+            if (!_modelManager.Panes.Any())
+                _modelManager.AddPane();
+            TemplateTextBox.Text = gi.Template;
+            templateFileBar.PathName = gi.TemplatePath;
+
+            IncludesTextBox.Text = string.Join(Environment.NewLine, gi.IncludePaths);
+        }
+
+
+        private void ExportInvocation(object sender, RoutedEventArgs e) => _projectManager.ExportProject();
+
+        #endregion
+
+        #region main loop
+
+        private void Avalon1_OnTextChanged(object sender, EventArgs e) => OnModelChanged();
+
+        private void OnModelChanged()
+        {
+            if (!_uiIsReady)
+                return;
+            try
+            {
+                _inputStream.OnNext(CollectInput());
+            }
+            catch (Exception exception)
+            {
+                Errors.Text = exception.Message;
+            }
         }
 
         private TimedOperation<ApplicationEngine> Render(EngineInputSet gi)
@@ -201,61 +341,39 @@ namespace TextrudeInteractive
                 IncludesTextBox.Text);
         }
 
-        public EngineOutputSet CollectOutput()
+        #endregion
+
+        #region settings
+
+        /// <summary>
+        ///     Loads any persisted settings and applies them
+        /// </summary>
+        private void LoadSettings()
         {
-            return new(
-                _outputManager.Panes.Select(b => new OutputPaneModel(b.Format, b.Name, b.OutputPath))
-            );
+            var settings = SettingsManager.ReadSettings();
+            LineNumbersOn = settings.LineNumbersOn;
+            TextSize = settings.FontSize;
+            WordWrapOn = settings.WrapText;
         }
 
-        private void OnModelChanged()
+        /// <summary>
+        ///     Persist settings
+        /// </summary>
+        private void PersistSettings()
         {
-            if (!_uiIsReady)
-                return;
-            try
+            //perist settings
+            var settings = new ApplicationSettings
             {
-                _inputStream.OnNext(CollectInput());
-            }
-            catch (Exception exception)
-            {
-                Errors.Text = exception.Message;
-            }
+                FontSize = TextSize,
+                LineNumbersOn = LineNumbersOn,
+                WrapText = WordWrapOn
+            };
+            SettingsManager.WriteSettings(settings);
         }
 
+        #endregion
 
-        private void LoadProject(object sender, RoutedEventArgs e) => _projectManager.LoadProject();
-
-
-        private void SaveProject(object sender, RoutedEventArgs e) => _projectManager.SaveProject();
-
-
-        private void SaveProjectAs(object sender, RoutedEventArgs e) => _projectManager.SaveProjectAs();
-
-
-        private void MainWindow_OnClosing(object sender, CancelEventArgs e) => _inputStream.OnCompleted();
-
-
-        public void SetUi(EngineInputSet gi)
-        {
-            DefinitionsTextBox.Text = string.Join(Environment.NewLine, gi.Definitions);
-            _modelManager.Clear();
-            foreach (var giModel in gi.Models)
-            {
-                var pane = _modelManager.AddPane();
-                pane.Format = giModel.Format;
-                pane.Text = giModel.Text;
-                pane.ModelName = giModel.Name;
-                pane.ModelPath = giModel.Path;
-            }
-
-            //ensure we start with at least one model to avoid confusing the user
-            if (!_modelManager.Panes.Any())
-                _modelManager.AddPane();
-            TemplateTextBox.Text = gi.Template;
-            templateFileBar.PathName = gi.TemplatePath;
-
-            IncludesTextBox.Text = string.Join(Environment.NewLine, gi.IncludePaths);
-        }
+        #region HelpMenu
 
         private void OpenBrowserTo(Uri uri)
         {
@@ -292,65 +410,6 @@ namespace TextrudeInteractive
         private void Questions(object sender, RoutedEventArgs e) =>
             OpenHome("issues/new?assignees=&labels=question&template=ask-a-question.md&title=Help");
 
-        private void ExportInvocation(object sender, RoutedEventArgs e) => _projectManager.ExportProject();
-
-        private void Avalon1_OnTextChanged(object sender, EventArgs e) => OnModelChanged();
-
-        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e) => _mainEditWindow.Register();
-
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-        private void SmallerFont(object sender, RoutedEventArgs e) => TextSize = Math.Max(TextSize - 2, 10);
-
-        private void LargerFont(object sender, RoutedEventArgs e) => TextSize = Math.Min(TextSize + 2, 36);
-
-        private void ToggleLineNumbers(object sender, RoutedEventArgs e) => LineNumbersOn = !LineNumbersOn;
-
-        private void ToggleWordWrap(object sender, RoutedEventArgs e) => WordWrapOn = !WordWrapOn;
-
-        public void SetOutputPanes(EngineOutputSet outputControl)
-        {
-            _outputManager.Clear();
-            foreach (var f in outputControl.Outputs)
-            {
-                var pane = _outputManager.AddPane();
-                pane.Format = f.Format;
-                pane.OutputPath = f.Path;
-                pane.OutputName = f.Name;
-            }
-
-            //ensure there is always at least one output - otherwise things can get confusing for the user
-            if (!_outputManager.Panes.Any())
-                _outputManager.AddPane();
-        }
-
-
-        private void AddModel(object sender, RoutedEventArgs e) =>
-            _modelManager.AddPane();
-
-        private void RemoveModel(object sender, RoutedEventArgs e) => _modelManager.RemoveLast();
-
-        private void AddOutput(object sender, RoutedEventArgs e) => _outputManager.AddPane();
-
-        private void RemoveOutput(object sender, RoutedEventArgs e) => _outputManager.RemoveLast();
-
-        private void ReloadAllInputs(object sender, RoutedEventArgs e)
-        {
-            _modelManager.ForAll(p => p.LoadIfLinked());
-            templateFileBar.LoadIfLinked();
-        }
-
-        private void SaveAllOutputs(object sender, RoutedEventArgs e)
-        {
-            _outputManager.ForAll(p => p.SaveIfLinked());
-        }
-
-        private void SaveAllInputs(object sender, RoutedEventArgs e)
-        {
-            _modelManager.ForAll(p => p.SaveIfLinked());
-            templateFileBar.SaveIfLinked();
-        }
+        #endregion
     }
 }
