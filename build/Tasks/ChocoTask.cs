4664154061714a168b5fefa914eedb8092644b9a
NeilMacMullen <neil.macmullen@outlook.com>
2021-04-02 18:50:18 +0100

    Better text

--- build/Tasks/ChocoTask.cs
+++ build/Tasks/ChocoTask.cs
@@ -12,7 +12,7 @@ namespace Build.Tasks
             TargetDir = context.ChocoDir;
             Client = "choco";
             base.Run(context);
-            Render.Line("Use 'choco push  chocolatey\textrude...   -s https://push.chocolatey.org/'");
+            Render.Line($"Use 'choco push {TargetDir}...   -s https://push.chocolatey.org/'".Yellow());
         }
     }
 }
